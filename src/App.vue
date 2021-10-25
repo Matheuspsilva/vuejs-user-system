74aaf8b06abc1833a734b28d6ac1707f0454292c
Matheus <matheuspsilva29@gmail.com>
2021-10-25 14:51:47 -0300

    Adicao navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/lista">Listagem</router-link>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active mx-1">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item mx-1">
+            <router-link to="/about">About</router-link>
+          <li class="nav-item mx-1">
+            <router-link to="/lista">Listagem</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
 
     <div class="container">
-    <router-view/>
-
+      <router-view/>
     </div>
-
 
   </div>
 </template>
@@ -26,12 +40,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
